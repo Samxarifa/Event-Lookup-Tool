f447645a4c8a12349d2a319ea2931a6da8d5a14d
Samxarifa <samuelhay9@gmail.com>
2025-02-25 15:41:27 +0000

    Works

--- index.ts
+++ index.ts
@@ -41,22 +41,91 @@ async function readFromFile(fileName: string) {
     return events;
 }
 
+function displayMainMenu() {
+    console.log('-'.repeat(50));
+    console.log(`1. Get Events by Entity Type ${selected[0] ? '(Selected)' : ''}`);
+    console.log(`2. Get Events Affecting a Field ${selected[1] ? '(Selected)' : ''}`);
+    console.log(`3. Get Events by Date Range ${selected[2] ? '(Selected)' : ''}`);
+    console.log('Enter. Submit');
+    console.log('q. Quit');
+    console.log('-'.repeat(50));
+}
 
 const events = await readFromFile('events.csv');
 
 let running = true;
+let filteredEvents: Event[] | undefined = undefined;
+let selected = [false,false,false];
+
+console.clear();
+console.log(`Event Lookup Tool (${events.length} Events)`);
+
 while (running) {
-    console.log(`Event Lookup Tool (${events.length} Events)`);
-    console.log('-'.repeat(50));
-    console.log('1. Get Events by Entity Name');
-    console.log('2. Get Events Affecting a Field');
-    console.log('3. Get Events by Date Range');
-    console.log('Enter. Submit');
-    console.log('q. Quit');
-    console.log('-'.repeat(50));
-    process.stdout.write('Enter your choice: ');
-    for await (const line of console) {
-        console.log(line);
+    displayMainMenu();
+    const input = prompt('Enter your choice: ');
+
+    switch (input) {
+        case 'q': {
+            running = false;
+            break;
+        }
+        case null: {
+            console.log(filteredEvents || events);
+            running = false;
+            break;
+        }
+        case '1': {
+            console.clear();
+            const type = prompt('Enter the Entity Type: ');
+            if (type === null) {
+                break;
+            }
+
+            if (filteredEvents) {
+                filteredEvents = filteredEvents.filter((event) => event.eventType === type);
+            }
+            else {
+                filteredEvents = events.filter((event) => event.eventType === type);
+            }
+            selected[0] = true;
+            break;
+        }
+        case '2': {
+            console.clear();
+            const field = prompt('Enter the Field Name: ');
+            if (field === null) {
+                break;
+            }
+
+            if (filteredEvents) {
+                filteredEvents = filteredEvents.filter((event) => event.fieldsUpdated?.includes(field));
+            }
+            else {
+                filteredEvents = events.filter((event) => event.fieldsUpdated?.includes(field));
+            }
+            selected[1] = true;
+            break;
+        }
+        case '3': {
+            console.clear();
+            const start = prompt('Enter the Start Date (YYYY-MM-DD HH:mm:ss:ms): ');
+            const end = prompt('Enter the End Date (YYYY-MM-DD HH:mm:ss:ms): ');
+            if (start === null || end === null) {
+                break;
+            }
+
+            const startDate = new Date(start);
+            const endDate = new Date(end);
+            
+            if (filteredEvents) {
+                filteredEvents = filteredEvents.filter((event) => event.timestamp >= startDate && event.timestamp <= endDate);
+            }
+            else {
+                filteredEvents = events.filter((event) => event.timestamp >= startDate && event.timestamp <= endDate);
+            }
+            selected[2] = true;
+            break;
+        }
     }
 }
 
